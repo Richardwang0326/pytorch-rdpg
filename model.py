5d5c77f68c511d844522ee32466bc3e5094fda73
Richard Wang <59532952+Richardwang0326@users.noreply.github.com>
2020-12-27 18:49:36 +0800

    Update model.py

--- model.py
+++ model.py
@@ -81,9 +81,7 @@ class Critic(nn.Module):
         out = self.relu(out)
         out = out.reshape(out.shape[0],1,-1)
         out = self.lstm(out)
-        print(type(out))
-        out = np.array(out)
+        out = torch.Tensor(out)
         out = out.reshape(out.shape[0],-1)
-        out = Variable(torch.Tensor(out))
         out = self.fc3(out)
         return out
