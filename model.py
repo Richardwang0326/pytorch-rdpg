c01ebe8c7f853bb2483853fc8f2c110d3e233c8a
Richard Wang <59532952+Richardwang0326@users.noreply.github.com>
2020-12-27 18:22:14 +0800

    Update model.py

--- model.py
+++ model.py
@@ -81,6 +81,7 @@ class Critic(nn.Module):
         out = self.relu(out)
         out = out.reshape(out.shape[0],1,-1)
         out = self.lstm(out)
+        out = np.array(out)
         out = out.reshape(out.shape[0],-1)
         out = self.fc3(out)
         return out
